bfe28f551fbcefa7dd1207b0f0d8b666733d6874
Khyquer Ronaldy <khyquer.oliveira@gmail.com>
2022-10-15 16:16:28 -0300

    Incluir o comportamento padrão do onSubmit

--- alura-studies/src/components/Form/index.tsx
+++ alura-studies/src/components/Form/index.tsx
@@ -5,12 +5,16 @@ import TimeField from "../Field/Time";
 import style from './Form.module.scss'
 
 const Form = () => {
-    const [time, setTime] = useState('00:00:00');
+    const [taskName, setTaskName] = useState('00:00:00');
+    const [taskTime, setTaskTime] = useState('00:00:00');
 
     return (
-        <form className={style.newTask}>
-            <TextField />
-            <TimeField value={time} />
+        <form
+            className={style.newTask}
+            onSubmit={(e:any) => {e.preventDefault()}}
+        >
+            <TextField value={taskName} />
+            <TimeField value={taskTime} />
             <Button>
                 Adicionar
             </Button>
