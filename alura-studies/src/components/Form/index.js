30acb51beff95b2aec8465254b8ce944c18971e9
Khyquer Ronaldy <khyquer.oliveira@gmail.com>
2022-10-15 13:49:00 -0300

    Criar form

---
+++ alura-studies/src/components/Form/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Button from "../Button";
+
+class Form extends React.Component {
+    render(){
+        return (
+            <Form>
+                <div>
+                    <label></label>
+                    <input></input>
+                </div>
+                <div>
+                    <label></label>
+                    <input></input>
+                </div>
+                <Button />
+            </Form>
+        )
+    }
+}
+
+export default Form;
